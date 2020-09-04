7beba9635a1d461947a78f373b0b662bbbb37838
welawrence543 <welawrence@bsu.edu>
2020-09-04 17:35:30 -0400

    Initial commit

--- romanconvertor.rb
+++ romanconvertor.rb
@@ -1,9 +1,48 @@
 def fromRoman(romanNumber)
-    # Replace the following line with the actual code!
-    raise NotImplementedError
+    length = romanNumber.length
+    for index in 0..(length-1)
+        if romanNumber[index] != "I" &&
+            romanNumber[index] != "V" &&
+            romanNumber[index] != "X" &&
+            romanNumber[index] != "L" &&
+            romanNumber[index] != "C" &&
+            romanNumber[index] != "D" &&
+            romanNumber[index] != "M"
+            raise TypeError
+        end
+    end
+
+    if romanNumber != romanNumber.upcase
+        raise TypeError
+    end
+
+    numerals = {"I" => 1, "V" => 5, "X" => 10, "L" => 50, "C" => 100,
+                  "D" => 500, "M" => 1000}
+
+    arabicValue = 0
+    for index in 0..(length - 1)
+        if (index > 0) && (numerals[romanNumber[index]] > numerals[romanNumber[index-1]])
+            arabicValue += numerals[romanNumber[index]] - 2 * numerals[romanNumber[index-1]]
+        else
+            arabicValue += numerals[romanNumber[index]]
+        end
+    end
+    return arabicValue
 end
 
 def toRoman(arabicNumber)
-    # Replace the following line with the actual code!
-    raise NotImplementedError
+    if arabicNumber < 1 || arabicNumber > 3999
+        raise RangeError
+    end
+
+    ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    thousands = ["", "M", "MM", "MMM"]
+
+    romanNumeral = thousands[arabicNumber / 1000] +
+        hundreds[(arabicNumber % 1000) / 100] +
+        tens[(arabicNumber % 100) / 10] +
+        ones[arabicNumber % 10]
+    return romanNumeral
 end
